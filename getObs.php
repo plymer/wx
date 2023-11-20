d791425a8485f088456b9b08dbfc8fb795fe0573
plymer <93949066+plymer@users.noreply.github.com>
2023-11-19 22:02:04 -0700

    quick update

--- getObs.php
+++ getObs.php
@@ -67,7 +67,6 @@ $siteElevM = $metaJSON["elev"];
 
 ob_start();
 $command = "/bin/python3 www/wx/sun-calc.py " . $siteLat . " " . $siteLon;
-echo "\n\n" . $command;
 passthru($command);
 $sunTimes = ob_get_contents();
 ob_end_clean();
