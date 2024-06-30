ac872abc0731b260aec22c08f00e6fbc6a8938ed
plymer <93949066+plymer@users.noreply.github.com>
2024-06-30 14:47:27 -0600

    wrap output as json for fetch API

--- utilities/getFX.php
+++ utilities/getFX.php
@@ -1,7 +1,17 @@
 <?php
 
-$office = strtolower($_GET["office"]);
-$bulletin = strtolower($_GET["bulletin"]);
+if (@$argc == 3) {
+    $office = strtolower($argv[1]);
+    $bulletin = strtolower($argv[2]);
+} elseif (@$argc != 0) {
+    echo "error: invalid arguments. this function requires two arguments - OFFICE and BULLETIN";
+    return;
+} else {
+
+	$office = strtolower($_GET["office"]);
+	$bulletin = strtolower($_GET["bulletin"]);
+
+}
 
 $referencePattern = "/(\n[-]{2,})\n?((.+)\n){1,}([-]{2,})/";
 
@@ -12,9 +22,11 @@ if ($bulletin == "focn45") {
 	//FO45 is stored as text, so no need to adjust things
 	$fxURL = "https://tgftp.nws.noaa.gov/data/raw/fo/focn45.cwwg..txt";
 
-	curl_setopt($ch, CURLOPT_URL, $fxURL);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$response = curl_exec($ch);
+	$response = file_get_contents($fxURL);
+
+	// curl_setopt($ch, CURLOPT_URL, $fxURL);
+	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	// $response = curl_exec($ch);
 
 	if (strlen($response) == 0) {
 		echo "There was an error retrieving the FOCN45 - you can try visiting <a style=\"color: blue;\" target=\"_blank\" href=\"{$fxURL}\">this link</a> to view it.";
@@ -31,10 +43,12 @@ if ($bulletin == "focn45") {
 	// API Link -----
 	// https://api.weather.gc.ca/collections/bulletins-realtime/items?f=json&lang=en-CA&limit=50&properties=datetime,identifier,issuer_code,issuing_office,type,url&issuer_code=CWTO&type=FP&datetime=2024-06-20
 	$fxURL = "https://weather.gc.ca/forecast/public_bulletins_e.html?Bulletin=" . $bulletin . "." . $office;
-	curl_setopt($ch, CURLOPT_URL, $fxURL);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	// curl_setopt($ch, CURLOPT_URL, $fxURL);
+	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-	$response = curl_exec($ch);
+	// $response = curl_exec($ch);
+
+	$response = file_get_contents($fxURL);
 
 	//do string stuff to $response
 
@@ -50,6 +64,11 @@ if ($bulletin == "focn45") {
 	$response = $fxContent;
 
 }
+
+// sometimes brad vrolijk includes accents in the fo45 because he's an idiot
+$response = htmlentities($response, ENT_IGNORE, "UTF-8");
+
+$response = json_encode(array("text" => $response));
 
 echo $response;
 
