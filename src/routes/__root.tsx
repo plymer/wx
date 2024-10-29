59161985a67d2942d9e13e1f54727be85b364878
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2024-10-28 21:36:34 -0600

    fixed refresh 404s with htaccess and updated build

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,67 +1,67 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
-// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Route = createRootRoute({
-  component: RootComponent,
+    component: RootComponent,
 });
 
 function RootComponent() {
-  return (
-    <>
-      <nav className="bg-neutral-100  flex justify-between px-4 place-items-center">
-        <img src="/site-icon.svg" className="w-10 h-10 inline mb-2" />
+    return (
+        <>
+            <nav className="bg-neutral-100  flex justify-between px-4 place-items-center">
+                <img src="/site-icon.svg" className="w-10 h-10 inline mb-2" />
 
-        <Link
-          className="px-6 py-3 text-center"
-          to="/public"
-          activeProps={{
-            className: "bg-neutral-800 text-white rounded-t-md",
-          }}
-        >
-          Public
-        </Link>
-        <Link
-          className="px-6 py-3 text-center"
-          to="/aviation"
-          activeProps={{
-            className: "bg-neutral-800 text-white rounded-t-md",
-          }}
-        >
-          Aviation
-        </Link>
-        <Link
-          className="px-6 py-3 text-center"
-          to="/observations"
-          activeProps={{
-            className: "bg-neutral-800 text-white rounded-t-md",
-          }}
-        >
-          Observations
-        </Link>
-        <Link
-          className="px-6 py-3 text-center"
-          to="/wxmap"
-          activeProps={{
-            className: "bg-neutral-800 text-white rounded-t-md",
-          }}
-        >
-          Weather Map
-        </Link>
-        <Link
-          className="px-6 py-3 text-center"
-          to="/outlooks"
-          activeProps={{
-            className: "bg-neutral-800 text-white rounded-t-md",
-          }}
-        >
-          Outlooks
-        </Link>
-      </nav>
+                <Link
+                    className="px-6 py-3 text-center"
+                    to="/public"
+                    activeProps={{
+                        className: "bg-neutral-800 text-white rounded-t-md",
+                    }}
+                >
+                    Public
+                </Link>
+                <Link
+                    className="px-6 py-3 text-center"
+                    to="/aviation"
+                    activeProps={{
+                        className: "bg-neutral-800 text-white rounded-t-md",
+                    }}
+                >
+                    Aviation
+                </Link>
+                <Link
+                    className="px-6 py-3 text-center"
+                    to="/observations"
+                    activeProps={{
+                        className: "bg-neutral-800 text-white rounded-t-md",
+                    }}
+                >
+                    Observations
+                </Link>
+                <Link
+                    className="px-6 py-3 text-center"
+                    to="/wxmap"
+                    activeProps={{
+                        className: "bg-neutral-800 text-white rounded-t-md",
+                    }}
+                >
+                    Weather Map
+                </Link>
+                <Link
+                    className="px-6 py-3 text-center"
+                    to="/outlooks"
+                    activeProps={{
+                        className: "bg-neutral-800 text-white rounded-t-md",
+                    }}
+                >
+                    Outlooks
+                </Link>
+            </nav>
 
-      <Outlet />
-      {/* <ReactQueryDevtools /> */}
-      {/* <TanStackRouterDevtools position="bottom-right" /> */}
-    </>
-  );
+            <Outlet />
+            {/* <ReactQueryDevtools /> */}
+            <TanStackRouterDevtools position="bottom-right" />
+        </>
+    );
 }
