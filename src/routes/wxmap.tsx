4fac4a72086bf0e7e7eba618b512670148d927d9
plymer <93949066+plymer@users.noreply.github.com>
2024-11-08 09:56:04 -0700

    remove the onclick for now

--- src/routes/wxmap.tsx
+++ src/routes/wxmap.tsx
@@ -14,20 +14,14 @@ export const Route = createFileRoute("/wxmap")({
 function WxMapComponent() {
   const geoMet = useGeoMetContext();
 
-  const handleClick = () => {
-    alert("clicked!");
-  };
+  // const handleClick = () => {
+  //   alert("clicked!");
+  // };
 
   return (
     <div className="bg-neutral-800 pt-2">
       <AnimationContextProvider>
-        <MapInstance
-          height="calc(100svh - 10.6rem)"
-          defaultLat={53}
-          defaultLon={-95}
-          defaultZoom={3.25}
-          onClick={handleClick}
-        >
+        <MapInstance height="calc(100svh - 10.6rem)" defaultLat={53} defaultLon={-95} defaultZoom={3.25}>
           <>
             <GeoMetLayer type="satellite" product={geoMet.subProduct} domain="west" belowLayer="wateroutline" />
             <GeoMetLayer type="satellite" product={geoMet.subProduct} domain="east" belowLayer="wateroutline" />
