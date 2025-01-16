3ef9c0b32cd54ad177e13de8143f0609c5afc738
Ryan Pimiskern <rpimiskern@gmail.com>
2025-01-15 17:55:27 -0700

    add pwa plugin and generate icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
+// plugins
+import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react()],
+  plugins: [TanStackRouterVite({}), react(), VitePWA({ registerType: "autoUpdate", manifest: false })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
-
