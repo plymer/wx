41d8da184878dd8dcb01449048f43135d5277b77
Ryan Pimiskern <rpimiskern@gmail.com>
2025-01-16 10:30:16 -0700

    1st attempt padding wxmap controls

--- src/components/WxMap.tsx
+++ src/components/WxMap.tsx
@@ -13,10 +13,10 @@ export default function WxMap() {
   return (
     <div className="bg-neutral-800 pt-2">
       <MapConfigContextProvider>
-        <MapInstance height="calc(100svh - 9.6rem)" defaultLat={53} defaultLon={-95} defaultZoom={3.25}>
+        <MapInstance height="calc(100svh - 11.6rem)" defaultLat={53} defaultLon={-95} defaultZoom={3.25}>
           <WeatherControls />
         </MapInstance>
-        <div className="w-full flex justify-center border-t-2  border-black bg-accent px-2 text-white">
+        <div className="w-full flex justify-center border-t-2  border-black bg-accent px-2 text-white max-md:pb-8">
           <AnimationControls />
         </div>
       </MapConfigContextProvider>
