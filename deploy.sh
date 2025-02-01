66a8305b22fc52b3d01c91fb4aabb377b27f7866
Ryan Pimiskern <rpimiskern@gmail.com>
2025-02-01 16:33:16 -0700

    a bit of wizardry

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 APP_NAME="beta.prairiewx.ca"
+APP_STAGING="prairiewx-staging"
 
 # Stop the CloudLinux Node.js app
 printf "\nStopping App: '$APP_NAME'..."
@@ -18,13 +19,23 @@ fi
 printf "\nPulling from git repo...\n"
 git pull
 
+# put the package*.json files into the root of the app
+printf "\nPreparing package*.json files\n"
+cp /home/ryanpimi/$APP_STAGING/package* /home/ryanpimi/$APP_NAME
+
 # Activate the virtual environment
 printf "\nActivating nodevenv...\n"
-source /home/ryanpimi/nodevenv/$APP_NAME/20/bin/activate && cd /home/ryanpimi/$APP_NAME
+source /home/ryanpimi/nodevenv/$APP_NAME/20/bin/activate && cd /home/ryanpimi/$APP_STAGING
 
 # Run the npm deploy command
 printf "\nDeploying app...\n"
 npm run deploy
+
+cp /home/ryanpimi/$APP_STAGING/dist/* /home/ryanpimi/$APP_NAME
+
+cd /home/ryanpimi/$APP_NAME
+
+npm install
 
 # Start the CloudLinux Node.js app
 printf "\nStarting app: '$APP_NAME'..."
