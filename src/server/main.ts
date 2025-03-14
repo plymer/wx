b6e238d2109315aa153ebadacc9742be832170a7
Ryan Pimiskern <rpimiskern@gmail.com>
2025-03-14 17:53:44 -0600

    resolving deploy to vps

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,6 +1,7 @@
 // third-party dependencies
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 import { readFileSync } from "fs";
 
 // endpoint imports
@@ -15,13 +16,15 @@ const isProd = process.env.NODE_ENV === "production";
 
 // get the content of the index.html so we can serve it from the root
 // TODO :: set this up to serve all files out of /dist just like in the HubWx implementation
-let html = readFileSync("index.html", "utf8");
+let html = readFileSync(isProd ? "/dist/index.html" : "index.html", "utf8");
 
 // inject vite dev server if we're not in production mode
 if (!isProd) html = injectViteDevServer(html);
 
 // create the server instance
-const app = new Hono();
+const app = new Hono({ strict: false });
+
+app.get("/assets/*", serveStatic({ root: isProd ? "./dist" : "./" }));
 
 // add our api routes here
 app.route("/api", geomet);
