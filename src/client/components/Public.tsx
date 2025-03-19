bf7f28d13821a0c3f5c6c7d6c5593cfefd414699
Ryan Pimiskern <rpimiskern@gmail.com>
2025-03-19 15:11:53 -0600

    buttons -> select for ui

--- src/client/components/Public.tsx
+++ src/client/components/Public.tsx
@@ -5,6 +5,7 @@ import { PublicBulletin } from "../lib/types";
 import useAPI from "../hooks/useAPI";
 import Button from "./ui/button";
 import { useEffect, useState } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 
 export default function Public() {
   const office = useOffice();
@@ -14,6 +15,14 @@ export default function Public() {
   const [productList, setProductList] = useState({});
   const [issuerCode, setIssuerCode] = useState("");
   const [productCode, setProductCode] = useState("");
+
+  // right now, this effect fires whenever we change the office OR the bulletin
+  // but we actually need different logic for each of those cases
+  // when we change bulletin, update the issuer code and product code because each office can cover multiple issuers
+
+  // BUT
+
+  // when we change the office, we should try to default the product selected back to the first product in the list, unless the current bulletin is still valid for the new office (i.e. in the case of the FOCN45CWWG product, which is available in multiple offices)
 
   useEffect(() => {
     const products = PUBLIC_FORECAST_CONFIG[office].products;
@@ -36,37 +45,51 @@ export default function Public() {
     bulletin: productCode,
   });
 
-  console.log(issuerCode, productCode);
-
   return (
     <div className="p-2 bg-neutral-800 text-white">
-      <div>
-        <h3 className="border-b-2 border-white mb-2">Office List:</h3>
-        {Object.keys(PUBLIC_FORECAST_CONFIG).map((key) => (
-          <Button
-            onClick={() => actions.setOffice(key as keyof typeof PUBLIC_FORECAST_CONFIG)}
-            variant={`${office === key ? "selected" : "secondary"}`}
+      <div className="flex items-center ">
+        <h3 className="inline-block">Office:</h3>
+        <div className="ms-2 min-w-fit w-full inline-block">
+          <Select
+            defaultValue={office}
+            onValueChange={(e) => actions.setOffice(e as keyof typeof PUBLIC_FORECAST_CONFIG)}
           >
-            {PUBLIC_FORECAST_CONFIG[key as keyof typeof PUBLIC_FORECAST_CONFIG].shortName}
-          </Button>
-        ))}
+            <SelectTrigger className=" text-black">
+              <SelectValue placeholder={productList[(productCode + issuerCode) as keyof typeof productList]} />
+            </SelectTrigger>
+            <SelectContent className="inline-block">
+              {Object.keys(PUBLIC_FORECAST_CONFIG).map((office, index) => (
+                <SelectItem key={index} value={office}>
+                  {PUBLIC_FORECAST_CONFIG[office as keyof typeof PUBLIC_FORECAST_CONFIG].shortName}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
       </div>
-      <div>
-        <h3 className="border-b-2 border-white mb-2">Product List:</h3>
-        {productList &&
-          Object.keys(productList).map((product) => (
-            <Button
-              key={product}
-              onClick={() => actions.setBulletin(product)}
-              variant={`${product === `${productCode}${issuerCode}` ? "selected" : "secondary"}`}
-            >
-              {productList[product as keyof typeof productList]}
-            </Button>
-          ))}
+      <div className="flex items-center border-y-2 border-white my-2 py-2">
+        <h3 className="inline-block">Products:</h3>
+        <div className="ms-2 min-w-fit w-full inline-block">
+          <Select defaultValue={productCode} onValueChange={(e) => actions.setBulletin(e as string)}>
+            <SelectTrigger className=" text-black">
+              <SelectValue placeholder={productList[(productCode + issuerCode) as keyof typeof productList]} />
+            </SelectTrigger>
+            <SelectContent className="inline-block">
+              {Object.keys(productList).map((product, index) => (
+                <SelectItem key={index} value={product}>
+                  {productList[product as keyof typeof productList]}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
       </div>
-      <h3 className="border-b-2 border-white mb-2">Product Text:</h3>
-      {Object.keys(productList).length !== 0 && data && data.data ? (
-        <pre className="overflow-y-scroll whitespace-pre-wrap" style={{ height: "calc(100svh - 15.3rem)" }}>
+
+      {productList[(productCode + issuerCode) as keyof typeof productList] && data && data.data ? (
+        <pre
+          className="overflow-y-scroll whitespace-pre-wrap text-sm md:px-6"
+          style={{ height: "calc(100svh - 11.2rem)" }}
+        >
           {data.data.trim()}
         </pre>
       ) : (
