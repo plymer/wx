69553fd17547dfd65e29da0b7a1b6dc954708477
Ryan Pimiskern <rpimiskern@gmail.com>
2025-06-13 07:18:52 -0600

    migrate radix sub-components to unified package

--- src/client/components/ui/Button.tsx
+++ src/client/components/ui/Button.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
+import { Slot as SlotPrimitive } from "radix-ui";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
@@ -50,7 +50,7 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+    const Comp = asChild ? SlotPrimitive.Slot : "button";
     return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
   },
 );
