d31495291bb6b2c0a7468e4fe431d90969d5a347
plymer <93949066+plymer@users.noreply.github.com>
2025-06-12 20:40:00 -0600

    Add hash-routing to the app for sharable links that work with iOS PWAs in the SPA paradigm (#37)
    * implement hash router for url state in pwa
    
    * removed 'git checkout dev' from deploy script
    
    * debugging redirect behaviour
    
    * debugging with route-relative nav
    
    * bad param being tested
    
    * bad switching click to route-relative too idk
    
    * more debugging and change back to pathname for replacement
    
    * trying to navigate to root instead
    
    * trying nav up one level
    
    * i was probably here before
    
    * add render counter for debug
    
    * switch to react-router-dom
    
    * changed main.tsx import
    
    * back to rr7
    
    * contemplating seppuku
    
    * lol using browser router yolo
    
    * add back debugging
    
    * hahahaha back to hashrouter kill me plz
    
    * remove extra hashes from navigate
    
    * working on local nginx/wsl config
    
    * functionality complete, yay
    
    * trying a sessionStorage solution for PWAs saved with hash path
    
    * add documentation about what/why for useEffect in <App/>

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,6 @@
 // third-party libraries
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 
 // ui components
 import Button from "@/components/ui/Button";
@@ -21,6 +23,52 @@ const appModesList: AppMode[] = Object.keys(APP_MODES_LIST).map((k) => k as AppM
 export const App = () => {
   const appMode = useAppMode();
   const setAppMode = useSetAppMode();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // handle app mode base on the URL hash path
+  // we are using the hash path to determine the app mode
+  // and allow us to create a sharable URL without breaking
+  // the iOS SPA PWA experience
+  // the nginx config on the server uses a 302 redirect to take any URL that points to /[appModesList]
+  // and convert it to a /#/[appModesList] URL, which is then handled by this code
+  useEffect(() => {
+    // this is a hack to allow iOS PWA users to have their app mode set when they first open the app
+    // this is necessary because when they add the app to their home screen,
+    // the app will reference whatever the their app mode was when they created the shortcut
+    // we're using sessionStorage to set a flag to ignore the hash path once and stop the
+    // redirect that would otherwise happen; this flag is cleared when the app is force-closed
+    // or the app is ended by iOS internal memory management
+    if (!sessionStorage.getItem("ignoreHashOnce")) {
+      if (location.pathname !== "/" && location.pathname !== "") {
+        // Remove the hash path and go to the appMode in localStorage
+        sessionStorage.setItem("ignoreHashOnce", "true");
+        navigate(`/${appMode}`, { replace: true });
+        return;
+      }
+    }
+
+    // get our hash mode from the URL path
+    // using HashRouter means that 'pathname' will be, compltely unintuitively(!)
+    //  the hash path, while 'hash' is actually empty (lol)
+    const hashMode = location.pathname.replace("/", "") as AppMode;
+
+    // if the hash mode is set, valid, and different from the current app mode, set the app mode
+    if (hashMode && hashMode !== appMode && appModesList.includes(hashMode)) {
+      setAppMode(hashMode);
+      // otherwise if there's no hashMode but we have an appMode, navigate to that app mode
+    } else if (!hashMode && appMode) {
+      navigate(`/${appMode}`, { replace: true });
+    }
+  }, [location.pathname]);
+
+  // handle the app mode when a user clicks on one of the app mode tabs
+  const handleSetAppMode = (mode: AppMode) => {
+    if (mode !== appMode) {
+      setAppMode(mode);
+      navigate(`/${mode}`, { replace: true });
+    }
+  };
 
   return (
     <main className="w-full max-w-(--breakpoint-2xl) mx-auto">
@@ -28,7 +76,12 @@ export const App = () => {
       <nav className="flex justify-between px-4 mt-2 place-items-center max-md:hidden">
         <img src="/site-icon.svg" className="w-10 h-10 inline me-2" />
         {appModesList.map((l, i) => (
-          <Button key={i} variant={"menuTab"} className={appMode === l ? "active " : ""} onClick={() => setAppMode(l)}>
+          <Button
+            key={i}
+            variant={"menuTab"}
+            className={appMode === l ? "active " : ""}
+            onClick={() => handleSetAppMode(l)}
+          >
             {APP_MODES_LIST[l].longName}
           </Button>
         ))}
@@ -38,7 +91,12 @@ export const App = () => {
       <nav className="flex justify-between px-2 place-items-center md:hidden ">
         <img src="/site-icon.svg" className="w-6 h-6 inline" />
         {appModesList.map((l, i) => (
-          <Button key={i} variant={"menuTab"} className={appMode === l ? "active " : ""} onClick={() => setAppMode(l)}>
+          <Button
+            key={i}
+            variant={"menuTab"}
+            className={appMode === l ? "active " : ""}
+            onClick={() => handleSetAppMode(l)}
+          >
             {APP_MODES_LIST[l].shortName}
           </Button>
         ))}
