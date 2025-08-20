51ea7f082b3e0624bc4236c8dfb55f3836cc9400
Ryan Pimiskern <rpimiskern@gmail.com>
2025-08-19 18:56:06 -0600

    attempt to fix bad import

--- src/server/endpoints/wxmap.ts
+++ src/server/endpoints/wxmap.ts
@@ -1,34 +1,34 @@
-import { Hono } from "hono";
-import { errorResponse, jsonResponse } from "../lib/utils";
-import { avwx } from "../main.js";
-import { validateParams } from "../lib/zod-validator";
-import { wxmapMetarSchema } from "../validationSchemas/wxmap.zod";
-// import * as fs from "fs";
+// import { Hono } from "hono";
+// import { errorResponse, jsonResponse } from "../lib/utils";
+// import { avwx } from "../main.js";
+// import { validateParams } from "../lib/zod-validator";
+// import { wxmapMetarSchema } from "../validationSchemas/wxmap.zod";
+// // import * as fs from "fs";
 
-const route = new Hono();
+// const route = new Hono();
 
-route.get("/wxmap/metars", async (c) => {
-  if (!avwx) {
-    console.error("[API] No avwx connection available.");
-    return errorResponse(c, "No avwx connection available.");
-  }
+// route.get("/wxmap/metars", async (c) => {
+//   if (!avwx) {
+//     console.error("[API] No avwx connection available.");
+//     return errorResponse(c, "No avwx connection available.");
+//   }
 
-  // fs.readFileSync(``, "utf-8");
+//   // fs.readFileSync(``, "utf-8");
 
-  // this will return the 'payload' of metars to popuplate the station data on the map
+//   // this will return the 'payload' of metars to popuplate the station data on the map
 
-  return jsonResponse(c, { message: "PAYLOAD - This endpoint is not implemented yet." });
-});
+//   return jsonResponse(c, { message: "PAYLOAD - This endpoint is not implemented yet." });
+// });
 
-route.get("/wxmap/metars/:id", validateParams("param", wxmapMetarSchema), async (c) => {
-  if (!avwx) {
-    console.error("[API] No avwx connection available.");
-    return errorResponse(c, "No avwx connection available.");
-  }
-  // this will allow for the incremental updating of metar data for all of the stations on the map via a payload-merge
-  const { id } = c.req.valid("param");
+// route.get("/wxmap/metars/:id", validateParams("param", wxmapMetarSchema), async (c) => {
+//   if (!avwx) {
+//     console.error("[API] No avwx connection available.");
+//     return errorResponse(c, "No avwx connection available.");
+//   }
+//   // this will allow for the incremental updating of metar data for all of the stations on the map via a payload-merge
+//   const { id } = c.req.valid("param");
 
-  return jsonResponse(c, { message: `Metars since the ${id}` });
-});
+//   return jsonResponse(c, { message: `Metars since the ${id}` });
+// });
 
-export default route;
+// export default route;
