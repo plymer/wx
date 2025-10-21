8ccc7969656a47fa31c5b8b9d9175d11cffd9bfe
Ryan Pimiskern <rpimiskern@gmail.com>
2025-10-20 20:48:23 -0600

    fixed build/structure

--- server/src/config/wms.config.ts
+++
@@ -1,9 +0,0 @@
-import { HOUR } from "../../../shared/lib/constants.js";
-
-export const GEOMET_GETCAPABILITIES: string =
-  "https://geo.weather.gc.ca/geomet/?lang=en&service=WMS&request=GetCapabilities&version=1.3.0&LAYERS_REFRESH_RATE=PT1M";
-
-export const EUMETSAT_GETCAPABILITIES =
-  "https://view.eumetsat.int/geoserver/wms?service=WMS&version=1.3.0&request=GetCapabilities";
-
-export const DATA_CUTOFF = 4 * HOUR; // hours
