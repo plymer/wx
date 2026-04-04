680c273e891abaea0cb1bb4b9467c1647b8a79bc
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2026-04-04 13:21:02 -0600

    add separate data processing path for dev (temporary)

---
+++ data-dev.config.cjs
@@ -0,0 +1,91 @@
+const path = require("path");
+
+const ENV_VARS = {
+  NODE_ENV: "production",
+  SQLITE_PATH: process.env.SQLITE_PATH,
+  OUTLOOK_DIR: process.env.OUTLOOK_DIR,
+  STATIC_DATA_DIR: process.env.STATIC_DATA_DIR,
+};
+
+module.exports = {
+  apps: [
+    {
+      name: "dev-aq-data-processing",
+      script: path.resolve(__dirname, "dist/server/data/aq-data.js"),
+      instances: 1,
+      exec_mode: "fork", // use fork for single-instance cron scripts
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "./logs/data/aq-error.log",
+      out_file: "./logs/data/aq-out.log",
+      merge_logs: true,
+      env: { ...ENV_VARS },
+      autorestart: false, // don't restart this script automatically
+      cron_restart: "*/10 * * * *",
+    },
+    {
+      name: "dev-build-station-catalog",
+      script: path.resolve(__dirname, "dist/server/data/stations.js"),
+      instances: 1,
+      exec_mode: "fork", // use fork for single-instance cron scripts
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "./logs/data/stations-error.log",
+      out_file: "./logs/data/stations-out.log",
+      merge_logs: true,
+      env: { ...ENV_VARS },
+      autorestart: false, // don't restart this script automatically
+      cron_restart: "0 0 * * *", // run once a day at midnight
+    },
+    {
+      name: "dev-public-alerts-fetch",
+      script: path.resolve(__dirname, "dist/server/data/public-alerts.js"),
+      instances: 1,
+      exec_mode: "fork",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "./logs/data/alerts-error.log",
+      out_file: "./logs/data/alerts-out.log",
+      merge_logs: true,
+      env: { ...ENV_VARS },
+      autorestart: false, // don't restart this script automatically
+      cron_restart: "* * * * *",
+    },
+    {
+      name: "dev-metars-processing",
+      script: path.resolve(__dirname, "dist/server/data/metars.js"),
+      instances: 1,
+      exec_mode: "fork", // use fork for single-instance cron scripts
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "./logs/data/metars-error.log",
+      out_file: "./logs/data/metars-out.log",
+      merge_logs: true,
+      env: { ...ENV_VARS },
+      autorestart: false, // don't restart this script automatically
+      cron_restart: "* * * * *",
+    },
+    {
+      name: "dev-tafs-processing",
+      script: path.resolve(__dirname, "dist/server/data/tafs.js"),
+      instances: 1,
+      exec_mode: "fork", // use fork for single-instance cron scripts
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "./logs/data/tafs-error.log",
+      out_file: "./logs/data/tafs-out.log",
+      merge_logs: true,
+      env: { ...ENV_VARS },
+      autorestart: false, // don't restart this script automatically
+      cron_restart: "*/10 * * * *", // AWC only updates cache every 10 min
+    },
+    {
+      name: "dev-sigmets-processing",
+      script: path.resolve(__dirname, "dist/server/data/sigmets.js"),
+      instances: 1,
+      exec_mode: "fork", // use fork for single-instance cron scripts
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "./logs/data/sigmets-error.log",
+      out_file: "./logs/data/sigmets-out.log",
+      merge_logs: true,
+      env: { ...ENV_VARS },
+      autorestart: false, // don't restart this script automatically
+      cron_restart: "* * * * *",
+    },
+  ],
+};
