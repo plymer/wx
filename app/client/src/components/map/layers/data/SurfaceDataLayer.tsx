5d3e113b65e3d8f7f8ae3e6ad9a22e487dc51a7d
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2026-06-28 08:54:22 -0600

    disable isotherm display until fully ready

--- app/client/src/components/map/layers/data/SurfaceDataLayer.tsx
+++ app/client/src/components/map/layers/data/SurfaceDataLayer.tsx
@@ -2,7 +2,7 @@ import { useZoom } from "@/stateStores/map/mapView";
 
 import { useFrame } from "@/stateStores/map/animation";
 import { Isobars } from "./sfc-data/Isobars";
-import { Isotherms } from "./sfc-data/Isotherms";
+// import { Isotherms } from "./sfc-data/Isotherms";
 import { Isodrosotherms } from "./sfc-data/Isodrosotherms";
 import { Popups } from "./sfc-data/Popups";
 import { Plots } from "./sfc-data/Plots";
@@ -18,7 +18,7 @@ export const SurfaceDataLayer = () => {
   return (
     <>
       <Isodrosotherms currentFrame={currentFrame} />
-      <Isotherms frameTime={frameTime} />
+      {/* <Isotherms frameTime={frameTime} /> */}
       <Isobars frameTime={frameTime} />
       <Plots frameTime={frameTime} />
       <Popups zoom={zoom} />
