7ee0463c06fc6be25028137c7ed73c9f2415b203
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2026-07-09 15:39:23 -0600

    forgot to set the initial time before the timeout runs

--- app/client/src/hooks/useClock.ts
+++ app/client/src/hooks/useClock.ts
@@ -10,6 +10,7 @@ function clockTick(
   const initialTime = new Date(Math.floor(Date.now() / MINUTE) * MINUTE).getTime();
   actions.setEndTime(initialTime);
   actions.setStartTime(initialTime - 3 * HOUR);
+  actions.setClockTime(initialTime);
 
   // our actual clock mechanism that updates the clock's time data in state
   // we are storing the NodeJS.Timeout that setInterval returns in state so we can
