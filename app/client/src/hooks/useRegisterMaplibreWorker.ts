3217b992da0307266c06e4a4e06051ddc5223ab9
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2026-08-08 08:29:26 -0600

    switch import string

--- app/client/src/hooks/useRegisterMaplibreWorker.ts
+++ app/client/src/hooks/useRegisterMaplibreWorker.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { setWorkerUrl, getWorkerUrl } from "maplibre-gl";
-import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?url";
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
 
 export const useRegisterMaplibreWorker = () => {
   useEffect(() => {
